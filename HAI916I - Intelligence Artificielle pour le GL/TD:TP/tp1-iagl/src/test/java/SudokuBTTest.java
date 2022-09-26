41e4d603aed3a5150d59cea5f72d90ace992d2ad
Romero <romero@MacBook-Pro-de-Romero.local>
2022-09-26 16:27:37 +0200

    rectif

---
+++ HAI916I - Intelligence Artificielle pour le GL/TD:TP/tp1-iagl/src/test/java/SudokuBTTest.java
@@ -0,0 +1,47 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author romero
+ *
+ */
+class SudokuBTTest {
+
+	/**
+	 * Test method for {@link SudokuBT#findSolution(int, int)}.
+	 */
+	@Test
+	void testFindSolution() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link SudokuBT#findSolutionAll(int, int)}.
+	 */
+	@Test
+	void testFindSolutionAll() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link SudokuBT#toString()}.
+	 */
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link SudokuBT#main(java.lang.String[])}.
+	 */
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
